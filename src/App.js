831f5a89b8f9d118877fa548eaa1505a0d6b04bc
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-28 16:43:14 -0400

    created two useEffect; one that controls the functionality of the slides to repeat itself once it reaches the end of the array or the beginning if sliding backwards; the second useEffect that controls the auto slider

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <section>
+    <section className="section">
       <div className="title">
         <h2>
           <span>/</span>reviews
@@ -38,10 +57,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
@@ -53,4 +72,12 @@ export default App;
 
 // 1) create an useState hook that will access the data array
 // 2) create an useState hook that will change the index value derived from the data array; default value will be zero for the initial index value
-// 3) in the jsx. you'll want to iterate over the state value people and use destructuring then create a return for the structure of your html tags
+// 3) in the jsx. you'll want to iterate over the state value people using the map() method and use destructuring then create a return for the structure of your html tags
+// 4) create a variable that will store the class name '____Slide' which we'll create conditions depending on index value and place that person as the active card
+// 5) create the first useEffect that which will run in two cases: whether the index changes or the people array changes
+// 6) in the first useEffect, first create a const that will equal to the last index of the array
+// 7) create conditions in which if the index value is negative, invoke setIndex to the last index value; if value is higher than length of the index, then setIndex to zero
+// 8) create a second useEffect for an auto slide functionality that will run whether the index changes
+// 9) this will invoke a setInterval function at 3000ms and increase the index value by one; and store it into a variable
+// 10) a cleanup function will be created to prevent multiple setInterval counters to run at the same time
+// 11) cleanup function will invoke the clearInterval and pass the variable slider
