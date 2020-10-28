1f45e39a51605ed52b86c7bde942a369af5c46df
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-28 15:39:08 -0400

    structured out the jsx; iterate over the data array and use destructuring for each prop; create conditionals based on the current index value which if the value changes then it will change the className for the <article> and switch to the previous/next slide

--- src/App.js
+++ src/App.js
@@ -7,10 +7,50 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  return <h2>slider project setup</h2>;
+  return (
+    <section>
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{title}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
 
 // 1) create an useState hook that will access the data array
 // 2) create an useState hook that will change the index value derived from the data array; default value will be zero for the initial index value
+// 3) in the jsx. you'll want to iterate over the state value people and use destructuring then create a return for the structure of your html tags
